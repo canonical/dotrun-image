2e0014bc1677577eb2a633725855a2c6495aff4d
Samuel Olwe <samuel@samolwe.ug>
2025-01-09 14:13:48 +0300

    Bump version

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@ assert sys.version_info >= (
 
 setup(
     name="dotrun-docker",
-    version="1.1.0",
+    version="1.2.0",
     packages=["dotrun_docker"],
     install_requires=[
         "ipdb",
